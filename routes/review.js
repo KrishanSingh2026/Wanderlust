0bb3a442e0dc32bbebb0216627a17aa929e547a2
krishansingh2026 <krishansingh.engineer@gmail.com>
2025-08-25 05:49:40 +0530

    Fix Routes

--- routes/review.js
+++ routes/review.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/review");
-const Listing = require("../models/listing.js");
 
 const reviewController = require("../controllers/reviews.js");
 
