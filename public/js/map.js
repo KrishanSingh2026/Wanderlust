b6769dc52c08766ca71bd60f9cac04a074ee762e
krishansingh2026 <krishansingh.engineer@gmail.com>
2025-09-14 19:33:10 +0530

    Delete unnecessary code

--- public/js/map.js
+++ public/js/map.js
@@ -1,19 +1,3 @@
-// var map = new maplibregl.Map({
-//   style: "https://tiles.stadiamaps.com/styles/outdoors.json",
-//   container: "map",
-//   center: geoData.coordinates, //Starting Position [lng, lat]
-//   zoom: 9,
-//   attributionControl: false,
-// });
-
-// new maplibregl.Marker({ color: "#ff6b6b" })
-//   .setLngLat([geoData.coordinates]) //listing.geoData.coordinates
-//   .setPopup(new maplibregl.Popup().setHTML("<h3>📍 Dehradun, Uttarakhand</h3>"))
-//   .addTo(map)
-//   .togglePopup();
-
-// public/js/map.js
-
 // Make sure maplibre is loaded
 if (typeof maplibregl !== "undefined" && coordinates) {
   const map = new maplibregl.Map({
